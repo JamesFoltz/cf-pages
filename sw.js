08146ebf1986185713eadfd19a65e78c9d25abe3
JamesF <34280247+JamesFoltz@users.noreply.github.com>
2025-04-04 10:17:24 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -39,3 +39,14 @@ self.addEventListener("fetch", (event) => {
     })()
   );
 });
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => 
+      Promise.all(
+        keys.filter(key => key !== cacheName)
+            .map(key => caches.delete(key))
+      )
+    )
+  );
+});
